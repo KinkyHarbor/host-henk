d0c31257e106b4637f9ebe7694aca51c47d1b857
Kinky Robbie <61333198+KinkyRobbie@users.noreply.github.com>
2020-02-23 21:44:43 +0100

    Traefik: KH demo backend config

---
+++ setup/traefik/10-kh-demo-backend.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Variables
+ENV_FILE=${HOME}/henk/.env
+TRAEFIK_DIR=${HOME}/henk/conf/traefik
+
+# Load env
+source "${ENV_FILE}"
+
+# Kinky Harbor: Frontend
+tee ${TRAEFIK_DIR:?}/kh-demo-backend.yml <<EOF
+http:
+  routers:
+    KHDemoBackend:
+      entryPoints: ["websecure"]
+      rule: Host(\`demo-api.${DEFAULT_DOMAIN:?}\`)
+      service: kh-demo-backend-${HOSTNAME:?}@docker
+      tls:
+        certResolver: le-tls
+EOF
